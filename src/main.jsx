17ab48cd86ecba4a80b7861ecc0845d59ab2eeee
aalampatil <aalam.workacc@gmail.com>
2025-12-29 23:02:06 +0530

    fix: Correct import statement for ThemeContextProvider to include file extension

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { WordContextProvider } from "./context/Word";
-import { ThemeContextProvider } from "./context/Theme";
+import { ThemeContextProvider } from "./context/Theme.jsx";
 import { AuthContextProvider } from "./context/Auth";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
